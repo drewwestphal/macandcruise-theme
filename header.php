b1a6bd6ab1f234e02ba5319a76fd22aff0cd96a1
Michaela McCann <kaebot@me.com>
2014-10-16 15:17:43 -0700

    updated bootstrap to include menu js, use hosted files

--- header.php
+++ header.php
@@ -5,7 +5,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<link rel="stylesheet" type="text/css" href="/wp-content/themes/MacAndCruise/css/bootstrap.min.css" />
+<!-- bootstrap css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<!-- jquery & bootstrap js -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- custom css -->
 <link rel="stylesheet" type="text/css" href="/wp-content/themes/MacAndCruise/css/custom.css" />
 <link href='http://fonts.googleapis.com/css?family=Lato:400,400italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
