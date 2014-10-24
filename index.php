f95198ea22b111d502ec9ad9b0c73cca239cd6ac
Michaela McCann <kaebot@me.com>
2014-10-23 21:19:56 -0700

    markup and loop for sponsors

--- index.php
+++ index.php
@@ -234,6 +234,29 @@
 			</div>
 		</div>
 	</section>
+	<section id="sponsors">
+		<div class="container">
+			<div class="col-xs-12 col-md-12">
+				<?php
+				$args = array(
+					'post_type' => 'sponsors',
+					'LIMIT'	=> '1'
+				);
+				$sponsors_query = new WP_Query( $args );
+				if ( $sponsors_query->have_posts() ) {
+					while ( $sponsors_query->have_posts() ) {
+						$sponsors_query->the_post();
+						?>
+						<h1><?php the_title(); ?></h1>
+						<?php
+						the_content();
+					}
+					wp_reset_postdata();
+				}
+				?>
+			</div>
+		</div>
+	</section>
 	
 </section>				
 
