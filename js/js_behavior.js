013099bf69c689d1c907b73d4c5fe290f6ac500f
Michaela McCann <kaebot@me.com>
2014-10-25 14:17:42 -0700

    implemented smooth scrolling

--- js/js_behavior.js
+++ js/js_behavior.js
@@ -105,6 +105,22 @@ $(document).ready(function(){
 		$(this).hide();
 		$('.wide-hidden').removeClass('wide-hidden');
 	});
+	
+	//smooth scroll
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 500);
+	        return false;
+	      }
+	    }
+	  });
+	});
 
 });
 
