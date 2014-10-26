cf65d1e36da6918df4a523bbae44a9e444de9905
Michaela McCann <kaebot@me.com>
2014-10-25 22:40:25 -0700

    changed email address

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
 <?php
-	
+
 if (isset($_REQUEST['email']) && isset($_REQUEST['name']) && isset($_REQUEST['comments']) && (strlen($_REQUEST['honeypot'])==0)) {
 
   $name = $_REQUEST['name'] ;
   $email = $_REQUEST['email'] ;
   $comments = $_REQUEST['comments'] ;
-  
-  mail("m@michaela.io", "Website Inquiry: Macaroni and Cruise","Name: $name\n Comments: $comments\n", "From: $email\r\nCC: $email\r\n'X-Mailer: PHP/' . phpversion()");
+  $from = "sf@jonathancoulton.com";
+    
+  mail($from, "Website Inquiry: Macaroni and Cruise","Name: $name\n Comments: $comments\n", "From: $email\r\nCC: $email\r\n'X-Mailer: PHP/' . phpversion()");
 
   echo "Thank you for getting in touch!";
 }
