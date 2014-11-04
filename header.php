21271a11f9ef57168e431429093752f1cf7caf7b
Michaela McCann <kaebot@me.com>
2014-11-03 17:02:05 -0800

    removed a comment

--- header.php
+++ header.php
@@ -44,7 +44,6 @@
 			<header id="header" role="banner" class="">
 				<nav class="navbar navbar-default" role="navigation">
 					<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
 						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 						    <span class="sr-only">Toggle navigation</span>
